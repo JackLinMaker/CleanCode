bbaf3077fe0928538bb7e06db168ed59d9721fec
jacklinmaker <jacklinmaker@gmail.com>
2013-08-10 16:32:44 +0800

    finish layout

---
+++ layout/BoldWidget.java
@@ -0,0 +1,22 @@
+package fitness.wikitest.widgets;
+
+import java.util.regex.*;
+
+public class BoldWidget extends ParentWidget {
+	public static final String REGEXP = "'''.+?'''";
+	private static final Pattern pattern = Pattern.compile("'''(.+?)'''", Pattern.MULTILINE + Pattern.DOTAIL);
+
+
+	public BoldWidget(PatternWidget parent, String text) throws Exception {
+		super(parent);
+		Matcher match = pattern.matcher(text);
+		match.find();
+		addChildWidgets(match.group(1));
+	}
+
+	public String render() throws Exception {
+		StringBuffer html = new StringBuffer("<b>");
+		html.append(chidHtml()).append("</b>");
+		return html.toString();
+	}
+}
